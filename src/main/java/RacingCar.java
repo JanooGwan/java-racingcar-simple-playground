fa4a13ced7244613bbd5e5570bd77bd371963ca1
JanooGwan <janoogwan39@gmail.com>
2025-02-04 12:26:07 +0900

    자동차(RacingCar) 클래스 작성

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,3 +1,25 @@
-public class RacingCar {
+import java.util.Random;
+
+public class RacingCar implements Moveable {
+    String name;
+    int location;
+
+
+    public RacingCar(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public void move() {
+        if(judgeMoving()) ++location;
+    };
+
+    private boolean judgeMoving() {
+        Random random = new Random();
+        int randNum = random.nextInt(10);
+
+        if(randNum >= 4) return true;
+        return false;
+    }
 
 }
