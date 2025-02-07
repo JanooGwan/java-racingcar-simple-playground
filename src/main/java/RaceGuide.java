993294a1a7bd7c9413dde05363eedd7ff7f60382
JanooGwan <janoogwan39@gmail.com>
2025-02-07 21:15:09 +0900

    RaceGuide 클래스 생성, 메소드 일부 작성

---
+++ src/main/java/RaceGuide.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class RaceGuide {
+    Scanner sc = new Scanner(System.in);
+
+    public void getInfo() {
+        getCarString();
+        getTryNum();
+        sendInfo();
+    }
+
+    private void getCarString() {
+        System.out.println("경주할 자동차 이름을 입력해주세요.");
+        System.out.println("쉼표(,)로 구분해서 입력해주세요.");
+        System.out.println("ex) neo,brie,brown\n");
+        System.out.println("입력 : ");
+        sc.nextLine();
+    }
+
+    private void elaborateString() {
+
+    }
+
+    private void getTryNum() {
+        sc.nextLine();
+        System.out.println("시도할 횟수를 입력해주세요.");
+        System.out.println("입력 : ");
+    }
+
+    private void sendInfo() {
+
+    }
+}
--- src/test/java/RaceManagerTest.java
+++ src/test/java/RaceManagerTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class RaceManagerTest {
     @Test
-    void 레이스_매니저_테스트() {
+    void 레이스_매니저_테스트_랜덤() {
         RaceManager rm = new RaceManager();
 
         rm.joinRacingCar("carA");
